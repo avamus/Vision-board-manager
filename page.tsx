39772cca94bf5f8fe0a6a2a0a13bad1826e6306d
avamus <lukassys2004@gmail.com>
2025-02-07 00:21:47 +0100

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { VisionDashboard } from "@/components/ui/vision-dashboard"
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-white">
+      <div className="max-w-[1400px] mx-auto">
+        <VisionDashboard />
+      </div>
+    </main>
+  )
+}
